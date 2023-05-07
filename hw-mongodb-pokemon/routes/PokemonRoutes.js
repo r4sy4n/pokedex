7abb6af68ddf6c74f140d12ae7c4bbba00c822aa
r4sy4n <124577832+r4sy4n@users.noreply.github.com>
2023-05-07 14:57:26 +0800

    Feat: implement $in operator to find type of pokemon

--- hw-mongodb-pokemon/routes/PokemonRoutes.js
+++ hw-mongodb-pokemon/routes/PokemonRoutes.js
@@ -7,13 +7,13 @@ router.get( '/', ( request, response ) => {
         // limit the number of documents
     let limit = parseInt(request.query.limit);
         // exec handle promises and executes the query
-    Pokemon.find().limit(limit).exec().then( res => {
+    Pokemon.find().limit(limit).exec().then( result => {
         if(limit){
         // Get a rate-limited number of pokemons
-            response.status( 200 ).send( res );
+            response.status( 200 ).send( result );
         }else{ 
         // GET list of all pokemons       
-            response.status( 200 ).send({ pokemons: res });
+            response.status( 200 ).send({ pokemons: result });
         };
     });
 });
@@ -29,7 +29,33 @@ router.get( '/:name', ( request, response ) => {
 
         Pokemon.findOne({ name : names }).then(pokemon => {
             console.log(pokemon);
-            response.status( 200 ).send({ pokemons: pokemon}) ;
+            if(pokemon){
+                response.status( 200 ).send({ pokemons: pokemon}) ;
+            }else {
+                response.status( 404 ).send( 'Pokemon not found' );
+            }
+        });
+    };
+});
+
+// Get all pokemons that match a type
+router.get( '/type/:type', ( request, response ) => { 
+    const type = request.params.type.toLowerCase();
+    if (type) {
+        let firstLetter = type.slice(0, 1);
+        let remaining = type.slice(1).toLowerCase();
+        let types = firstLetter.toUpperCase() + remaining;
+        // $in operator selects the documents where the value of a field equals any value in the specified array
+        Pokemon.find({ type: { $in: [ types ] } }).then((pokemons) => {
+            console.log(pokemons);
+            if (!pokemons || pokemons.length === 0) {
+                response.status( 404 ).send( 'No Pokemon found for this type' );
+            }else{
+                response.status( 200 ).send({ 
+                    count: pokemons.length,
+                    pokemons: pokemons 
+                });
+            } 
         });
     };
 });
