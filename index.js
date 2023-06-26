11c88756cce921adbef5f8b530512c61691b14ab
r4sy4n <russell.ramiro@gmail.com>
2023-06-26 16:05:58 +0800

    implement dotenv

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
+const Secret_Password = process.env.Secret_Password;
 
 const app = express();
 app.use( bodyParser.json() );
 app.use( cors() );
 
-mongoose.connect('mongodb://127.0.0.1:27017/pokemondb');
+mongoose.connect(`mongodb+srv://russellramiro:${Secret_Password}@cluster0.rz5gupu.mongodb.net//pokemondb`);
 
 const baseURL = '/api/v1/pokemons';
 
